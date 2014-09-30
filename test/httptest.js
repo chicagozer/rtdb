0127e329f738aa6f86fe2a1fcb15520a8398d067
Jim Mittler <jim.mittler@xtime.com>
2014-09-30 00:42:07 -0700

    more coverage testing

--- test/httptest.js
+++ test/httptest.js
@@ -6,6 +6,9 @@
 "use strict";
 var assert = require('assert');
 var request = require('request');
+var EventSource = require('eventsource');
+
+
 
 var Rtdb = require('../rtdb');
 describe('Suite', function() {
@@ -21,27 +24,50 @@ describe('Suite', function() {
         myrtdb.stop(done);
     });
 
-    describe('HTTP', function() {
-		
-        it('about', function(done) {
+   	
+	    describe('HTTP', function() {
+			
+			
+	        it('about', function(done) {
 
-            request.get({
-                url: 'http://localhost:9001/about',
-                auth: {
-                    user: 'admin',
-                    pass: 'chang3m3'
-                },
-                json: true
-            }, function(error, response, body) {
-                if (!error && response.statusCode === 200) {
-                    done();
-                } else if (error) {
-                    done(error);
-                } else {
-                    done(new Error('[' + response.statusCode + ']:' + response.body));
-                }
-            });
-        });
+	            request.get({
+	                url: 'http://localhost:9001/about',
+	                auth: {
+	                    user: 'admin',
+	                    pass: 'chang3m3'
+	                },
+	                json: true
+	            }, function(error, response, body) {
+	                if (!error && response.statusCode === 200) {
+	                    done();
+	                } else if (error) {
+	                    done(error);
+	                } else {
+	                    done(new Error('[' + response.statusCode + ']:' + response.body));
+	                }
+	            });
+	        });	
+			
+		
+	        it('echo', function(done) {
+
+	            request.get({
+	                url: 'http://localhost:9001/db/admin/echo',
+	                auth: {
+	                    user: 'admin',
+	                    pass: 'chang3m3'
+	                },
+	                json: true
+	            }, function(error, response, body) {
+	                if (!error && response.statusCode === 200) {
+	                    done();
+	                } else if (error) {
+	                    done(error);
+	                } else {
+	                    done(new Error('[' + response.statusCode + ']:' + response.body));
+	                }
+	            });
+	        });
 		
         it('index', function(done) {
 
@@ -321,6 +347,50 @@ describe('Suite', function() {
             /*jslint unparam: false */
         });
 		
+		
+	    it('stream Collection:Apples', function(done) {
+
+			var es = new EventSource('http://localhost:9001/db/stream?view=ec537999-60a5-41f3-9036-fcd3d5356ae2');
+			
+	        es.addEventListener("ec537999-60a5-41f3-9036-fcd3d5356ae2",
+	        function(event) {
+				es.close();
+				done();
+	        }, false);
+	    });
+		
+	    it('stream Collection:Parcels', function(done) {
+
+			var es = new EventSource('http://localhost:9001/db/collections/e08e31fa-f414-4f2f-b067-6bce67fae7b0/views/18823768-0635-49bc-9053-ac2f212d066b/stream');
+			
+	        es.addEventListener("18823768-0635-49bc-9053-ac2f212d066b",
+	        function(event) {
+				es.close();
+				done();
+	        }, false);
+	    });
+		
+	    it('stream Collection:notfound', function(done) {
+
+			var es = new EventSource('http://localhost:9001/db/collections/notfound/views/18823768-0635-49bc-9053-ac2f212d066b/stream');
+			
+	       done();
+	    });
+		
+	    it('stream Collection:multiple', function(done) {
+
+			var es = new EventSource('http://localhost:9001/db/stream?view=ec537999-60a5-41f3-9036-fcd3d5356ae2&view=18823768-0635-49bc-9053-ac2f212d066b');
+			
+			done();
+	    });
+		
+	    it('stream Collection:delta', function(done) {
+
+			var es = new EventSource('http://localhost:9001/db/stream?view=ec537999-60a5-41f3-9036-fcd3d5356ae2&delta=true');
+			
+			done();
+	    });
+		
         it('stream Collection:Parcel:(view not found)', function(done) {
 
             request.get({
@@ -460,6 +530,94 @@ describe('Suite', function() {
 
             request.get({
                 url: 'http://localhost:9001/db/collections/e08e31fa-f414-4f2f-b067-6bce67fae7b0/views/18823768-0635-49bc-9053-ac2f212d066b/ticket',
+                auth: {
+                    user: 'admin',
+                    pass: 'chang3m3'
+                },
+                json: true
+            }, function(error, response, body) {
+                /*jslint unparam: true */
+                if (!error && response.statusCode === 200) {
+                    done();
+                } else if (error) {
+                    done(error);
+                } else {
+                    done(new Error('[' + response.statusCode + ']:' + response.body));
+                }
+            });
+            /*jslint unparam: false */
+        });
+		
+        it('Collection:Parcel reload', function(done) {
+
+            request.post({
+                url: 'http://localhost:9001/db/collections/e08e31fa-f414-4f2f-b067-6bce67fae7b0/load',
+                auth: {
+                    user: 'admin',
+                    pass: 'chang3m3'
+                },
+                json: true
+            }, function(error, response, body) {
+                /*jslint unparam: true */
+                if (!error && response.statusCode === 200) {
+                    done();
+                } else if (error) {
+                    done(error);
+                } else {
+                    done(new Error('[' + response.statusCode + ']:' + response.body));
+                }
+            });
+            /*jslint unparam: false */
+        });
+		
+        it('Collection:Parcel reload (notfound)', function(done) {
+
+            request.post({
+                url: 'http://localhost:9001/db/collections/notfound/load',
+                auth: {
+                    user: 'admin',
+                    pass: 'chang3m3'
+                },
+                json: true
+            }, function(error, response, body) {
+                /*jslint unparam: true */
+                if (!error && response.statusCode === 404) {
+                    done();
+                } else if (error) {
+                    done(error);
+                } else {
+                    done(new Error('[' + response.statusCode + ']:' + response.body));
+                }
+            });
+            /*jslint unparam: false */
+        });
+		
+        it('help', function(done) {
+
+            request.get({
+                url: 'http://localhost:9001/help/intro',
+                auth: {
+                    user: 'admin',
+                    pass: 'chang3m3'
+                },
+                json: true
+            }, function(error, response, body) {
+                /*jslint unparam: true */
+                if (!error && response.statusCode === 200) {
+                    done();
+                } else if (error) {
+                    done(error);
+                } else {
+                    done(new Error('[' + response.statusCode + ']:' + response.body));
+                }
+            });
+            /*jslint unparam: false */
+        });
+		
+        it('demo', function(done) {
+
+            request.get({
+                url: 'http://localhost:9001/demo/parcels',
                 auth: {
                     user: 'admin',
                     pass: 'chang3m3'
