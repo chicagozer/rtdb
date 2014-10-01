d337cd5fb27fab50ed79b85b0862f29e51859dc7
Jim Mittler <jim.mittler@xtime.com>
2014-10-01 09:55:51 -0700

    linting

--- cfs/hstream.js
+++ cfs/hstream.js
@@ -1,4 +1,4 @@
-// © 2014 by Rheosoft. All rights reserved. 
+// © 2014 by Rheosoft. All rights reserved.
 // Licensed under the RTDB Software License version 1.0
 /*jslint node: true, white: true, nomen: true */
 /*jshint laxbreak: true */
@@ -6,93 +6,93 @@
 var nforce = require('nforce');
 
 function HStream() {
-	return this;
+    return this;
 }
 
 var org = nforce.createConnection({
-	clientId : process.env.NFORCE_CID,
-	clientSecret : process.env.NFORCE_CSECRET,
-	redirectUri : "http://localhost:3000/oauth/_callback",
-	"environment" : "production"
+    clientId: process.env.NFORCE_CID,
+    clientSecret: process.env.NFORCE_CSECRET,
+    redirectUri: "http://localhost:3000/oauth/_callback",
+    "environment": "production"
 
 });
 
 var http = require('http');
 
 var headers = {
-	'Content-Type' : 'application/json'
+    'Content-Type': 'application/json'
 };
 
 var delay = 0;
 
 var options = {
-	host : process.env.OPENSHIFT_NODEJS_IP || 'localhost',
-	port : process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 9001,
-	path : '/db/collections/c6e6dda4-e715-4be0-aa36-8ea08a53a3a2/documents',
-	method : 'POST',
-	headers : headers
+    host: process.env.OPENSHIFT_NODEJS_IP || 'localhost',
+    port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 9001,
+    path: '/db/collections/c6e6dda4-e715-4be0-aa36-8ea08a53a3a2/documents',
+    method: 'POST',
+    headers: headers
 };
 
 org.authenticate({
-	username : process.env.NFORCE_USER,
-	password : process.env.NFORCE_PWD
+    username: process.env.NFORCE_USER,
+    password: process.env.NFORCE_PWD
 }, function(err, oauth) {
-	if (err) {
-		global.logger.log('error', 'HStream.authenticate -  ', err);
-		return;
-	}
+    if (err) {
+        global.logger.log('error', 'HStream.authenticate -  ', err);
+        return;
+    }
 
-	var str = org.stream({ topic: 'PushTopic3', oauth:oauth});
+    var str = org.stream({
+        topic: 'PushTopic3',
+        oauth: oauth
+    });
 
-	str.on('connect', function() {
-		global.logger.log('debug',
-				'HStream.authenticate - connected to "PushTopic3"');
-		// reset the connection delay 
-		delay = 0;
+    str.on('connect', function() {
+        global.logger.log('debug',
+            'HStream.authenticate - connected to "PushTopic3"');
+        // reset the connection delay
+        delay = 0;
 
-	});
+    });
 
-	str.on('error', function(error) {
-		global.logger.log('error', 'HStream.authenticate -  ', error);
-		setTimeout(org.authenticate,delay);
-		if (delay === 0) {
-			delay = 60000;
-		}
-	});
+    str.on('error', function(error) {
+        global.logger.log('error', 'HStream.authenticate -  ', error);
+        setTimeout(org.authenticate, delay);
+        if (delay === 0) {
+            delay = 60000;
+        }
+    });
 
-	str.on('data', function(data) {
-		global.logger.log('debug', 'HStream.authenticate - received:', data);
+    str.on('data', function(data) {
+        global.logger.log('debug', 'HStream.authenticate - received:', data);
 
-		// Setup the request. The options parameter is
-		// the object we defined above.
-		var req = http.request(options, function(res) {
+        // Setup the request. The options parameter is
+        // the object we defined above.
+        var req = http.request(options, function(res) {
 
-			
-			res.setEncoding('utf-8');
-			res.on('end', function() {
-				global.logger.log('debug', 'HStream.authenticate - posted');
-			});
-			
-			res.on('data', function(data) {
-			});
 
-			res.on('error', function(err) {
-				global.logger.log('error', 'HStream.authenticate - posting:',err);
-			});
-			
-		});
+            res.setEncoding('utf-8');
+            res.on('end', function() {
+                global.logger.log('debug', 'HStream.authenticate - posted');
+            });
 
-		req.on('error', function(e) {
-			global.logger.log('error', 'HStream.post -  ', e);
-		});
+            res.on('data', function(data) {});
 
-		req.write(JSON.stringify(data.sobject));
-		req.end();
+            res.on('error', function(err) {
+                global.logger.log('error', 'HStream.authenticate - posting:', err);
+            });
 
-	});
+        });
+
+        req.on('error', function(e) {
+            global.logger.log('error', 'HStream.post -  ', e);
+        });
+
+        req.write(JSON.stringify(data.sobject));
+        req.end();
+
+    });
 
 });
 
 module.exports = HStream;
-
-
